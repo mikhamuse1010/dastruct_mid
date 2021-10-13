d9540f03b62f5ba3c51a38f95fe954310a8057fe
Aurielle Li <89785862+yuiyeyo@users.noreply.github.com>
2021-10-13 13:04:42 +0700

    Create archive.cpp

---
+++ archive.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include <string> 
+#include <math.h> // For Extended class
+using namespace std;
+
+class Calculator
+{
+    public:
+        void addhistory();
+        void clearhistory();
+        void deletelast();
+        void showhistory();
+    private:
+        queue <string> history;
+};
+
+// For extended operators other than + - * /
+class Extended {
+    public:
+        // Trigonometry
+        double sin();
+        double cos();
+        double tan();
+        // Numeric Operations
+        double sqRoot();
+        double exp();
+        double log();
+        double ln();
+};
+
+// For basic operators
+class Operators {
+    public:
+        double Addition(double a, double b);
+        double Substraction(double a, double b);
+        double Multiplication(double a, double b);
+        double Division(double a, double b);
+};
+
+int Operators::Addition(double a, double b) {
+    return (a + b);
+}
+
+int Operators::Substraction(double a, double b) {
+    return (a - b);
+}
+
+int Operators::Multiplication(double a, double b) {
+    return (a * b);
+}
+
+double Operators::Division(double a, double b) {
+    return double(a / b);
+}
+
+// Main function
+int main() {
+    //const int MAX_INPUT = 100;
+    Operators op;
+    Extended ex;
+    Calculator calc;
+    string eq;
+    double ans;
+    int temp'
+    //int i = MAX_INPUT;
+
+    //char input[MAX_INPUT];
+
+    getline(cin,eq);
+    temp=eq.find("*");
+    if (temp != string::npos)
+    {
+        
+    }
+    
+    // ini gw bingung mau digimanain lagi 
+    // how to mengolah input-nya. stuck masihan
+    // makanya gw templating class nya dulu
+
+    return 0;
+}
